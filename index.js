23c26c062206927fe7b9eb5947846f96abb14488
soruj <mathsoruj@gmail.com>
2023-02-06 23:51:29 +0600

    add

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 // This is a input Nambar
 const numbar = 5;
 
-function mindGame(numbar) {
+function mindGame( numbar) {
+
     const multiplication = numbar * 3;
     const addition = multiplication + 10;
     const division = addition / 2;
@@ -10,73 +11,77 @@ function mindGame(numbar) {
 
     return subtraction;
 }
-console.log(mindGame(numbar))
+console.log(mindGame(numbar));
 
-// const texts = 'Soruj Mahmud';
 
-// function evenOdd(texts) {
-//     const text = texts.length;
+// This is String Input  start here .
+const texts = 'Soruj Mahmud';
 
-//     if (text % 2 === 0) {
-//        return 'even'
-//    }
-//     else {
-//         return 'odd'
-//    }
+function evenOdd(texts) {
+    const text = texts.length;
 
-// }
-// console.log(evenOdd(texts));
+    if (text % 2 === 0) {
+       return 'even'
+   }
+    else {
+        return 'odd'
+   }
 
-// const num = -15;
+}
+evenOdd(texts);
+// This is String Input end here .
 
-// function isLGSeven(num) {
-//     const sub = num - 7;
-//     if (sub < 7) {
-//         return sub;
-//     }
-//     else if (7 < sub) {
-//         return sub*2
-//     }
-// }
+
+const num = 15;
+
+function isLGSeven(num) {
+    const sub =  num - 7;
+    if (sub < 7) {
+        return sub;
+    }
+    else{
+        return sub*2
+    }
+}
 // console.log(isLGSeven(num));
 
 
-// const arrayNumaber = [2, -5, -7, -13];
+// negative array counting  function start herer
+const arrayNumaber = [1, 2, 5];
 
-// function findingBadData(arrayNumaber) {
+function findingBadData(arrayNumaber) {
 
-//     let element = 0;
+    let element = 0;
 
-//     for (let i = 0; i < arrayNumaber.length; i++) {
-//         const index = arrayNumaber[i];
-//         if (index<0) {
-//             element++;
-//         }
-//     }
-//     return element;
-// }
+    for (let i = 0; i < arrayNumaber.length; i++) {
+        const index = arrayNumaber[i];
+        if (index<0) {
+            element++;
+        }
+    }
+    return element;
+}
+findingBadData(arrayNumaber);
 
-// console.log(findingBadData(arrayNumaber));
+// negative array counting  function end herer
 
+// gemsToDiamond function start here
+function gemsToDiamond(first,second,third) {
+    const first1 = first * 21;
+    const second2 = second * 32;
+    const third3 = third * 43;
 
-
-
-// function gemsToDiamond(first,second,third) {
-//     const first1 = first * 21;
-//     const second2 = second * 32;
-//     const third3 = third * 43;
-
-//     const totalGems = first1 + second2 + third3;
+    const totalGems = first1 + second2 + third3;
     
-//     if (totalGems < 1000) {
-//         console.log(totalGems);
-//     }
-//     else if (2000 < totalGems) {
-//         const gems = totalGems - 2000;
-//         console.log(gems);
-//     }
+    if (totalGems < 1000) {
+        return totalGems;
+    }
+    else if (2000 < totalGems) {
+        const gems = totalGems - 2000;
+        return gems;
+    }
 
-// }
+}
 
-// gemsToDiamond(100, 5, 1);
-
+gemsToDiamond(100, 5, 1);
+// gemsToDiamond function end here
